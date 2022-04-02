2e9c6b4b0ce5ebec7580fb4531082d6b7bd0aaab
stojanovic98m <stojanovic98m@gmail.com>
2022-04-02 15:15:39 +0200

    initial push

---
+++ smd-theme-switcher/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./styles.css";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1> sta ima </h1>
+    </div>
+  );
+}
+
+export default App;
